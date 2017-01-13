3eda6ce888ad7dbdfb323a2136513b05d2dc3168
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-13 08:10:07 -0800

    Will sort answers by upvotes

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,7 @@ export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
   notes: DS.attr(),
-  answers: DS.hasMany('answer', { async: true})
+  answers: DS.hasMany('answer', { async: true}),
+  sorting: ['votes:desc'],
+  sorted_answers: Ember.computed.sort('answers', 'sorting')
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -2,11 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return Ember.RSVP.hash({
-      question: this.store.findRecord('question', params.question_id),
-      answers: this.store.query('answer', {  question: params.question_id })
-    });
-
+    return this.store.findRecord('question', params.question_id);
   },
 
   actions: {
