8263245cff5275760512d96b4acab693b355c395
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-13 10:57:46 -0800

    import at top of files, clean up indentation

--- app/routes/tag.js
+++ app/routes/tag.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Route.extend({
   model(params) {
-    var promise = this.store.findAll('question').then( function( questionsArray ) {
-          return questionsArray.filter( function( item, index, enumerable) {
-             return item.get('tag') === params.name;
-          });
-       })
+    var promise = this.store.findAll('question').then(function(questionsArray) {
+      return questionsArray.filter(function(item) {
+          return item.get('tag') === params.name;
+      });
+    });
 
-       return DS.PromiseArray.create({
-          promise: promise
-       });
+    return DS.PromiseArray.create({
+      promise: promise
+    });
   }
 });
+
+//This was a mess to figure out : )
