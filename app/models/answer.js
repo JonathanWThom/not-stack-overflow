8cc50705615caa993410a48f3f50f816dc5cb2a1
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-13 06:53:28 -0800

    Add votes feature to answers with default value

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
+  votes: DS.attr({defaultValue: 0}),
   answer: DS.belongsTo('question', { async: true})
 });
